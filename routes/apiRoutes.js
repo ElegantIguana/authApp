103648b6d1070d2287c81b45889e52f158eb1352
Armand Guim <armandguim3@gmail.com>
2018-03-06 18:17:55 -0500

    initial commit; pre-existing project

---
+++ routes/apiRoutes.js
@@ -0,0 +1,40 @@
+const db = require("../models");
+
+const passport = require("../config/passport");
+
+module.exports = function(app){
+    app.post("/api/login", passport.authenticate("local"), (req, res)=>{
+        res.json("/members");
+    });
+
+    app.post("/api/signup", (req, res)=>{
+        console.log(req.body);
+        db.User.create({
+            email: req.body.email,
+            password: req.body.password
+        }).then(()=>{
+            res.redirect(307, "/api/login");
+        }).catch(function(err){
+            console.log(err);
+            res.json(err);
+        });
+    });
+    //route for logging out
+    app.get("/logout", (req, res)=>{
+        req.logout();
+        res.redirect("/");
+        
+    })
+    app.get("/api/user_data", (req, res) => {
+        if (!req.user) {
+            res.json({});
+        }
+        else {
+            res.json({
+                email: req.user.email,
+                id: req.user.id
+            });
+        }
+    });
+
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const passport = require("./config/passport");
+
+const PORT = process.env.PORT || 8080;
+
+let app = express();
+const db = require("./models");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+app.use(session({ secret: "keyboard cat", resave: true, 
+saveUninitialized: true}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//require our routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+db.sequelize.sync().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`listening on port: ${PORT}`);
+    });
+});
